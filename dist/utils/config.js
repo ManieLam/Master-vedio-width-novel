22924fc9b0178e2a1ac220be9459d6e978c80a6e
linmoney <516520664@qq.com>
2017-07-21 21:21:04 +0800

    图文小说类播音器模板v1.0

---
+++ dist/utils/config.js
@@ -0,0 +1,9 @@
+const APP_ID = 'wx025c737dd743e8b0'
+const INVALID_TOKEN = 'illegal_access_token'
+const API_HOST = "https://yyzz.wpweixin.com/"
+
+module.exports = {
+    APP_ID,
+    INVALID_TOKEN,
+    API_HOST,
+}
